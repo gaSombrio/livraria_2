7f6dd169ebb294468a69eefcaee1f013a5d3aa0d
gaSombrio <sombrio.ga@gmail.com>
2026-06-16 14:13:49 -0300

    adiciona modelo Compra e registra no admin

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from django.contrib.admin import ModelAdmin, register
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Editora, Livro, User
+from core.models import Autor, Categoria, Compra, Editora, Livro, User
 
 
 @register(Autor)
@@ -22,6 +22,13 @@ class CategoriaAdmin(ModelAdmin):
     search_fields = ('descricao',)
     list_filter = ('descricao',)
     ordering = ('descricao',)
+    list_per_page = 10
+
+
+@register(Compra)
+class CompraAdmin(ModelAdmin):
+    list_display = ('usuario', 'status')
+    ordering = ('usuario', 'status')
     list_per_page = 10
 
 
